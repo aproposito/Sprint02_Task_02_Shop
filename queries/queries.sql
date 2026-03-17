90472cc6d9c32b1702c34acabbf8395b3eb9b0d4
aproposito <aproposito@gmail.com>
2026-03-17 18:16:04 +0100

    Update queries.sql

--- queries/queries.sql
+++ queries/queries.sql
@@ -11,7 +11,11 @@ Select
 From producto;
 
 -- 3. Llista totes les columnes de la taula producto.
-Select *
+Select 
+	codigo,
+	nombre,
+	precio,
+	codigo_fabricante
 From producto;
 
 -- 4. Llista el nom dels productes, el preu en euros (precio_eur) i el preu en dòlars estatunidencs (precio_usd) amb un tipus de canvi de 1 € = 1 $.
